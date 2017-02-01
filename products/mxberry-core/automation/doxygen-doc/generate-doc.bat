77eed2ca19355e3b8c4d6cd23ce248fecb0d76fc
pgagarinov <pgagarinov@gmail.com>
2017-02-01 12:16:07 +0300

    Documentation generation scripts were updated to produce output directly in docs folder and create README.md in the root folder

--- products/mxberry-core/automation/doxygen-doc/generate-doc.bat
+++ products/mxberry-core/automation/doxygen-doc/generate-doc.bat
@@ -1,5 +1,6 @@
 set locDir=%~dp0
-rmdir /s /q "%locDir%..\..\doc"
+rmdir /s /q "%locDir%..\..\..\..\docs"
+xcopy /f /y "%locDir%README.md" "%locDir%..\..\..\..\README.md"
 python %locDir%prep4doxymat.py %locDir%..\.. %locDir%..\..\..\mxberry-core-doxygen-prep %locDir%..\..\..\mxberry-core-doxygen-garbage
 for /f %%i in ('cd') do set curDir=%%i
 cd %locDir%

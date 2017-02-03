5c5a8a007bcbc302f806d7e6c5ffe1a65cb7078f
pgagarinov <pgagarinov@gmail.com>
2017-02-03 14:10:14 +0300

    The generated Doxygen documentation now refers to Matlab packages as "Packages" instead of "Namespaces"

--- docs/menudata.js
+++ docs/menudata.js
@@ -1,8 +1,8 @@
 var menudata={children:[
 {text:"Main Page",url:"index.html"},
-{text:"Namespaces",url:"namespaces.html",children:[
-{text:"Namespace List",url:"namespaces.html"},
-{text:"Namespace Members",url:"namespacemembers.html",children:[
+{text:"Packages",url:"namespaces.html",children:[
+{text:"Packages",url:"namespaces.html"},
+{text:"Package Functions",url:"namespacemembers.html",children:[
 {text:"All",url:"namespacemembers.html",children:[
 {text:"a",url:"namespacemembers.html#index_a"},
 {text:"b",url:"namespacemembers.html#index_b"},
